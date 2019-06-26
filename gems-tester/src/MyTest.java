5d8dc29212f5006b8e07ad40ee2023773be2925d
Nathan Loria <nathandloria@gmail.com>
2019-06-26 12:27:36 -0400

    added errors

--- gems-tester/src/MyTest.java
+++ gems-tester/src/MyTest.java
@@ -1,7 +1,12 @@
-public class MyTest
+public class MyTest{
   public static void main(String[] args){
-    System.out.println("hey";
-    System.out.println("Hello World!")
+    System.out.println("hey");
+    System.out.println("Hello World!");
   }
 }
+static void test() {
+  System.out.println("hello");
+
+static void test2() {
+  System.out.println("hello");
 }
