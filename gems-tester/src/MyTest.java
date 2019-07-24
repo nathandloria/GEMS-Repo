8aa00b782721164255972130be7740b9819ff397
Nathan Loria <nathandloria@gmail.com>
2019-07-24 16:17:16 -0400

    added database support

--- gems-tester/src/MyTest.java
+++ gems-tester/src/MyTest.java
@@ -1,12 +1,6 @@
-public class MyTest{
-  public static void main(String[] args){
+public class MyTest {
+  public static void main(String[] args) {
     System.out.println("hey");
     System.out.println("Hello World!");
   }
-}
-static void test() {
-  System.out.println("hello");
-
-static void test2() {
-  System.out.println("hello");
 }
