1eead66b25155d8a5dd4497926910b094056c2d3
Nathan Loria <lorian@allegheny.edu>
2019-12-26 16:48:08 -0500

    further improvements

--- gems-src/src/jgrader/scraper/scraper.java
+++ gems-src/src/jgrader/scraper/scraper.java
@@ -25,7 +25,8 @@ public class scraper {
     String messageEncoded = encode(message);
 
     try {
-      Document doc = Jsoup.connect("https://stackoverflow.com/search?q=" + messageEncoded + "+Java+error").get();
+      System.out.println("https://stackoverflow.com/search?q=Java+error%3A+" + messageEncoded);
+      Document doc = Jsoup.connect("https://stackoverflow.com/search?q=Java+error%3A+" + messageEncoded).get();
       Elements resultLinks = doc.select(("a[class=question-hyperlink]"));
       for (Element link : resultLinks) {
         if (link.attr("title").contains(message)) {
