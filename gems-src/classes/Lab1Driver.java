6680bec3fa1cff81b6ab12a1cbb47624c1640ac5
Nathan Loria <lorian@allegheny.edu>
2019-12-27 14:47:36 -0500

    updater update

--- gems-src/classes/Lab1Driver.java
+++ gems-src/classes/Lab1Driver.java
@@ -1,38 +1,5 @@
-import java.util.Scanner;
-
-public class Lab1Driver
-{
-   public static void main(String[] args)
-   {
-       int fTemp = getFarenheit();
-       double cTemp = calculateCelsius();
-      displayInstructions();
-      getFarenheit();
-      calculateCelsius();
-      displayResults();
-   }
-   public static void displayInstructions()
-   {
-       System.out.println("This program will convert farenheit to celsius");
-       System.out.println("after you enter a farenheit temperature");
-   }
-   public static int getFarenheit()
-   {
-       System.out.println("Please enter your temperature below");
-       Scanner keyboard = new  Scanner(System.in);
-       return keyboard.nextInt();
-   }
-   public static double calculateCelsius(int farenheit)
-   {
-       return (5 * (farenheit - 32) / 9.0);
-   }
-   public static void displayResults(int fTemp, double cTemp, long l)
-   {
-       System.out.println("Your farenheit temperature is " + fTemp);
-       System.out.printf("Your celsius temperature is ", + cTemp);
-       System.out.println("Thank you for using the program!");
-       System.out.println("The rest of the world needs to conform to imperial measurments, cuz 'merica");
-       return;
-
-   }
+public class Lab1Driver {
+  public static void main(String[] args) {
+    int l = 10.0;
+  }
 }
--- gems-src/src/jgrader/updatedb/updater.java
+++ gems-src/src/jgrader/updatedb/updater.java
@@ -3,10 +3,13 @@ package jgrader.updatedb;
 import java.sql.*;
 import java.util.Arrays;
 import java.util.ArrayList;
+import jgrader.parse.Dbdataparser;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class updater {
   private String[] errorMessages;
-  private int index;
+  private int index = 0;
   private static int low;
   private static int high;
   private static int mid;
@@ -28,66 +31,33 @@ public class updater {
       String url = "jdbc:mysql://" + rhost + ":" + lport + "/gems";
       String dbUser = "gems";
       String dbPasswd = "gemsdb2019";
+
       try {
         Class.forName(driverName).getDeclaredConstructor().newInstance();
       } catch(Exception ex) {
         System.out.println(ex);
       }
+
       conn = DriverManager.getConnection(url, dbUser, dbPasswd);
       Statement stat = conn.createStatement();
       ResultSet rest = null;
 
-      for(int d = 0; d < emessages.size(); d++) {
-        int count = 0;
-        rest = stat.executeQuery("SELECT * FROM eMessagesTable");
-        while (rest.next()) {
-          count++;
-        }
-        int i = 0;
-        errorMessages = new String[count];
-        rest = stat.executeQuery("SELECT * FROM eMessagesTable");
-        while (rest.next()) {
-          errorMessages[i] = rest.getString("emessages");
-          i++;
-        }
-        Arrays.sort(errorMessages);
-        index = searchBinary(errorMessages, emessages.get(d));
+      Dbdataparser bdp = new Dbdataparser();
+      bdp.setStringArrays();
+      errorMessages = bdp.getOgErrorArr();
 
+      for (int i = 0; i < emessages.size(); i++) {
+        index = searchBinary(errorMessages, emessages.get(i));
         if (index == -1) {
-
           try (PreparedStatement pstmt = conn.prepareStatement("INSERT INTO eMessagesTable VALUES(?)")) {
-            pstmt.setString(1, emessages.get(d));
+            pstmt.setString(1, emessages.get(i));
             pstmt.executeUpdate();
           } catch(Exception ex) {
             System.out.println(ex);
           }
-
-          // stmt.executeUpdate("INSERT INTO eMessagesTable VALUES (\"" + emessage + "\")");
-        } else {
-          boolean cont = false;
-          String sub1 = emessages.get(d).substring(0, 5);
-          String sub2 = emessages.get(d).substring(emessages.get(d).length() - 5, emessages.get(d).length());
-          for (int g = 0; g < errorMessages.length; g++) {
-            if (errorMessages[g].contains(sub1) && errorMessages[g].contains(sub2)) {
-              cont = true;
-            }
-          }
-          if (cont == false) {
-
-            try (PreparedStatement pstmt = conn.prepareStatement("INSERT INTO eMessagesTable VALUES(?)")) {
-              pstmt.setString(1, emessages.get(d));
-              pstmt.executeUpdate();
-            } catch(Exception ex) {
-              System.out.println(ex);
-            }
-
-            // stmt.executeUpdate("INSERT INTO eMessagesTable VALUES (\"" + emessage + "\")");
-          }
         }
       }
-      rest.close();
       stat.close();
-      conn.close();
     } catch (Exception e) {
       System.out.println(e);
     }
@@ -95,31 +65,25 @@ public class updater {
 
   public static int searchBinary(String[] array, String str) {
     low = 0;
-    high = array.length - 1;
+		high = array.length - 1;
 
-    while (low <= high) {
-      mid = (low + high) / 2;
+		while (low <= high) {
+			mid = (low + high) / 2;
 
-      if (array[mid].contains("cannot find symbol") && str.contains("cannot find symbol")) {
-				return mid;
-			} else if (array[mid].contains("non-static variable") && str.contains("non-static variable")) {
-				return mid;
-			} else if (array[mid].contains("non-static method") && str.contains("non-static method")) {
-				return mid;
-			} else if (array[mid].contains("should be declared in a file named") && str.contains("should be declared in a file named")) {
-				return mid;
-			} else if (array[mid].contains("cannot be applied to given types") && str.contains("cannot be applied to given types")) {
+			if (array[mid].contains("(.*)")) {
+				Pattern p = Pattern.compile(array[mid]);
+				if (p.matcher(str).matches()) {
+					return mid;
+				}
+			}
+			if (array[mid].compareTo(str) < 0) {
+				low = mid + 1;
+			} else if (array[mid].compareTo(str) > 0) {
+				high = mid - 1;
+			} else {
 				return mid;
 			}
-
-      if (array[mid].compareTo(str) < 0) {
-        low = mid + 1;
-      } else if (array[mid].compareTo(str) > 0) {
-        high = mid - 1;
-      } else {
-        return mid;
-      }
-    }
-    return -1;
+		}
+		return -1;
   }
 }
